57e6bfd69b3abeef6ad1670c10ea8bbf4ae49de3
Amit Patil <gpamitranjit@gmail.com>
2021-09-09 23:24:19 +0530

    simple kafka producer with keys

---
+++ simple -kafka-producer-with-keys/src/main/java/com/amit/app/ProducerDemoWithCallback.java
@@ -0,0 +1,59 @@
+package com.amit.app;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoWithCallback {
+
+    final static Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        String boostrapServers = "127.0.0.1:9092";
+
+//        Create properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "first_topic";
+
+        for (int i = 0; i < 10; i++) {
+
+            String key = "id_" + i;
+            String value = "Hello World " + i;
+
+//        create producer record
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+
+//        send data -asynchronous
+            producer.send(record, (recordMetadata, e) -> {
+                if (e == null) {
+                    logger.info("Received new metadata: \n" +
+                            "Topic: " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp());
+                } else {
+                    logger.error("Error while producing: " + e);
+                }
+            }).get();       // this makes synchronous, don't use .get() api in production!
+
+//        flush data
+            producer.flush();
+        }
+//        flush data & close the producer
+        producer.close();
+    }
+}
